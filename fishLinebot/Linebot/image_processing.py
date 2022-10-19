cf63862364776eed762921b3a2d7889b132da77a
CHIEH <44966962+tuna2766@users.noreply.github.com>
2022-10-20 00:43:48 +0800

    Add files via upload

--- fishLinebot/Linebot/image_processing.py
+++ fishLinebot/Linebot/image_processing.py
@@ -8,9 +8,35 @@ def image_processing(image_path):
     r = p.stdout.read().decode("utf-8")
     idx = r.find('Predicted')
     r = r[idx:len(r) - 1]
+
+    fish_name = 'null'
+    fish_num = -1
+    if r.count('ALB') > 0 :
+        fish_name = 'ALB'
+        fish_num = r.count('ALB')
+    elif r.count('BET') > 0 :
+        fish_name = 'BET'
+        fish_num = r.count('BET')
+    elif r.count('DOL') > 0 :
+        fish_name = 'DOL'
+        fish_num = r.count('DOL')
+    elif r.count('LAG') > 0 :
+        fish_name = 'LAG'
+        fish_num = r.count('LAG')
+    elif r.count('Shark') > 0 :
+        fish_name = 'Shark'
+        fish_num = r.count('Shark')
+    elif r.count('YFT') > 0 :
+        fish_name = 'YFT'
+        fish_num = r.count('YFT')
+    elif r.count('OTHER') > 0 :
+        fish_name = 'OTHER'
+        fish_num = r.count('OTHER')
+
+
     image_name = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(4))
     image_name = image_name.upper()+'.jpg'
 
     p = subprocess.call("mv /home/nvidia/Desktop/workspace/fishLinebot/predictions.jpg /home/nvidia/Desktop/workspace/fishLinebot/static/" + image_name, shell=True)
  
-    return r, image_name
+    return fish_name, fish_num, r, image_name
